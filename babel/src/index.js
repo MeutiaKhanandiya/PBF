99e24db2abcaf3481981543c7e3008505b647521
MeutiaKhanandiya <khanandyam@gmail.com>
2020-03-05 23:52:02 +0700

    praktikum5

---
+++ babel/src/index.js
@@ -0,0 +1,10 @@
+function getName(){
+    var nama;
+    if(false){
+        var nama = 'Meutia';
+        console.log(nama);
+    }else{
+        console.log(nama123);
+    }
+    }
+    getName();
